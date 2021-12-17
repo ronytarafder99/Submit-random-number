a3eaa9f1b9e1aa855af38bfbe56febaf7eff0f52
Rony T <ronytarafder99@gmail.com>
2021-12-17 18:32:51 +0600

    Add search and delete option

--- includes/register_page.php
+++ includes/register_page.php
@@ -66,221 +66,235 @@ function wps_theme_func_faq()
 </div>
 <?php }
 
-function wps_theme_func_settings(){ ?>
-<div class="wrap">
-    <div id="icon-users" class="icon32"></div>
-    <h2>Random Number List Table Page</h2>
+function wps_theme_func_settings(){
+    global $wpdb;
+
+    $table = new we_Table_Example_List_Table();
+    $table->prepare_items();
+
+    $message = '';
+    if ('delete' === $table->current_action()) {
+        $message = '<div class="updated below-h2" id="message"><p>Item Deleted</p></div>';
+    }
+    ?>
+    <div class="wrap">
+
+    <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
+    <h2><?php _e('Random Number', 'cltd_example')?></h2>
+    <?php echo $message; ?>
+
+    <form id="persons-table" method="GET">
+        <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+        <?php $table->search_box('search', 'search_id'); ?>
+        <?php $table->display() ?>
+    </form>
+
+    </div>
     <?php
-        $exampleListTable = new Example_List_Table();
-        $exampleListTable->prepare_items();
-        $exampleListTable->display_search_box();
-        $exampleListTable->load_notices();
-        $exampleListTable->display(); 
-        ?>
-</div>
-<?php
 }
 
 // WP_List_Table is not loaded automatically so we need to load it in our application
 if( ! class_exists( 'WP_List_Table' ) ) {
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
 }
 
 /**
  * Create a new table class that will extend the WP_List_Table
  */
-class Example_List_Table extends WP_List_Table
+class we_Table_Example_List_Table extends WP_List_Table
 {
     /**
-     * Prepare the items for the table to process
-     *
-     * @return Void
+     * [REQUIRED] You must declare constructor and give some basic params
      */
-    public function prepare_items()
+    function __construct()
     {
-        $columns = $this->get_columns();
-        $hidden = $this->get_hidden_columns();
-        $sortable = $this->get_sortable_columns();
+        global $status, $page;
 
-        $data = $this->table_data();
-        usort( $data, array( &$this, 'sort_data' ) );
-
-        $this->process_bulk_action();
-
-        $paged = isset($_REQUEST['paged']) ? ($per_page * max(0, intval($_REQUEST['paged']) - 1)) : 0;
-        $orderby = (isset($_REQUEST['orderby']) && in_array($_REQUEST['orderby'], array_keys($this->get_sortable_columns()))) ? $_REQUEST['orderby'] : 'id';
-        $order = (isset($_REQUEST['order']) && in_array($_REQUEST['order'], array('asc', 'desc'))) ? $_REQUEST['order'] : 'asc';
-
-        $perPage = 20;
-        $currentPage = $this->get_pagenum();
-        $totalItems = count($data);
-
-        $this->set_pagination_args( array(
-            'total_items' => $totalItems,
-            'per_page'    => $perPage
-        ) );
-
-        $data = array_slice($data,(($currentPage-1)*$perPage),$perPage);
-
-        $this->_column_headers = array($columns, $hidden, $sortable);
-        $this->items = $data;
+        parent::__construct(array(
+            'singular' => 'person',
+            'plural' => 'persons',
+        ));
+        
     }
 
     /**
-     * Override the parent columns method. Defines the columns to use in your listing table
+     * [REQUIRED] this is a default column renderer
      *
-     * @return Array
+     * @param $item - row (key, value array)
+     * @param $column_name - string (key)
+     * @return HTML
      */
-
-
-    public function get_columns()
+    function column_default($item, $column_name)
     {
-        $columns = array(
-            'cb'               => '<input type="checkbox" />',
-            'id'          => 'ID',
-            'title'       => 'Number',
-            // 'description' => 'Description',
-            'year'        => 'Time',
-            // 'director'    => 'Director',
-            // 'rating'      => 'Rating'
+        return $item[$column_name];
+    }
+
+    /**
+     * [OPTIONAL] this is example, how to render specific column
+     *
+     * method name must be like this: "column_[column_name]"
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_age($item)
+    {
+        return '<em>' . $item['time'] . '</em>';
+    }
+
+    /**
+     * [OPTIONAL] this is example, how to render column with actions,
+     * when you hover row "Edit | Delete" links showed
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_email($item)
+    {
+        // links going to /admin.php?page=[your_plugin_page][&other_params]
+        // notice how we used $_REQUEST['page'], so action will be done on curren page
+        // also notice how we use $this->_args['singular'] so in this example it will
+        // be something like &person=2
+        $actions = array(
+            'delete' => sprintf('<a href="?page=%s&action=delete&id=%s">%s</a>', $_REQUEST['page'], $item['id'], __('Delete', 'cltd_example')),
         );
 
+        return sprintf('%s %s',
+            $item['id'],
+            $this->row_actions($actions)
+        );
+    }
+
+    /**
+     * [REQUIRED] this is how checkbox column renders
+     *
+     * @param $item - row (key, value array)
+     * @return HTML
+     */
+    function column_cb($item)
+    {
+        return sprintf(
+            '<input type="checkbox" name="id[]" value="%s" />',
+            $item['id']
+        );
+    }
+
+    /**
+     * [REQUIRED] This method return columns to display in table
+     * you can skip columns that you do not want to show
+     * like content, or description
+     *
+     * @return array
+     */
+    function get_columns()
+    {
+        $columns = array(
+            'cb' => '<input type="checkbox" />', //Render a checkbox instead of text
+            'email' => __('ID', 'cltd_example'),
+            'term' => __('Number', 'cltd_example'),
+            'age' => __('Time', 'cltd_example'),
+        );
         return $columns;
     }
 
-    public function column_id( $item ) {
-        $actions = array(
-            'delete' => sprintf( '<a href="?page=%s&action=%s&search-term=%s">' . __( 'Delete', 'search-analytics' ) . '</a>', $_REQUEST['page'], 'delete', $item['id'] ),
-            'view'   => sprintf( '<a href="?page=%s&search-term=%s">' . __( 'View Details', 'search-analytics' ) . '</a>', $_REQUEST['page'], $item['id'] )
-        );
-
-        return sprintf( '<a href="?page=%1$s&search-term=%2$s">%2$s</a> %4$s', $_REQUEST['page'], $item['id'], $item['term'], $this->row_actions( $actions ) );
-    }
-
-    public function column_cb( $item ) {
-        return sprintf(
-            '<input type="checkbox" name="search-term[]" value="%s" />', $item['id']
-        );
-    }
-
-    function get_bulk_actions() {
-        return array(
-            'delete' => __( 'Delete', 'search-analytics' )
-        );
-    }
-
-    function process_bulk_action() {
-        global $wpdb;
-
-        if ( 'delete' === $this->current_action() && ! empty( $_GET['search-term'] ) ) {
-            $table_name = $wpdb->prefix . 'wesoftpress_random';
-            $terms_to_delete = (array) $_GET['search-term'];
-            $terms_placeholders = implode( ',', array_fill( 0, count( $terms_to_delete ), '%d' ) );
-
-            $wpdb->query(
-                  $wpdb->prepare(
-                        "DELETE FROM $table_name WHERE id IN ($terms_placeholders)",
-                      $terms_to_delete
-                  )
-            );
-
-            wp_die( sprintf( '%s <a href="%s">%s</a>',
-                    __( 'Items deleted!', 'search-analytics' ),
-                    add_query_arg( 'result', 'deleted', remove_query_arg( array(
-                        'action',
-                        'search-term'
-                    ) ) ),
-                    __( 'Go Back!', 'search-analytics' )
-                )
-            );
-        }
-
-    }
-
-    
     /**
-     * Define which columns are hidden
+     * [OPTIONAL] This method return columns that may be used to sort table
+     * all strings in array - is column names
+     * notice that true on name column means that its default sort
      *
-     * @return Array
+     * @return array
      */
-    public function get_hidden_columns()
-    {
-        return array();
-    }
-
-    /**
-     * Define the sortable columns
-     *
-     * @return Array
-     */
-    public function get_sortable_columns()
+    function get_sortable_columns()
     {
         $sortable_columns = array(
-            'title' => array('title', true),
-            'id' => array('id', false),
-            'year' => array('year', false),
+            'term' => array('term', true),
+            'email' => array('id', false),
+            'age' => array('time', false),
         );
         return $sortable_columns;
     }
 
     /**
-     * Get the table data
+     * [OPTIONAL] Return array of bult actions if has any
      *
-     * @return Array
+     * @return array
      */
-    private function table_data()
+    function get_bulk_actions()
     {
-        global $wpdb;
-        $table_name = $wpdb->prefix . 'wesoftpress_random';
-        $data =  $wpdb->get_results( "SELECT * FROM $table_name ORDER BY ID DESC");
-        $data = json_decode(json_encode($data), true);
-        
-        foreach ($data as $value){ 
-            $data1[] = array(
-                'id'          => $value['id'],
-                'title'       => $value['term'],
-                // 'description' => 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-                'year'        => $value['time'],
-                // 'director'    => 'Frank Darabont',
-                // 'rating'      => '9.3'
-                );
-        } 
-
-        return $data1;
+        $actions = array(
+            'delete' => 'Delete'
+        );
+        return $actions;
     }
 
     /**
-     * Define what data to show on each column of the table
-     *
-     * @param  Array $item        Data
-     * @param  String $column_name - Current column name
-     *
-     * @return Mixed
+     * [OPTIONAL] This method processes bulk actions
+     * it can be outside of class
+     * it can not use wp_redirect coz there is output already
+     * in this example we are processing delete action
+     * message about successful deletion will be shown on page in next part
      */
-    public function column_default( $item, $column_name ){
-        switch( $column_name ) {
-            case 'id':
-            case 'title':
-            case 'description':
-            case 'year':
-            case 'director':
-            case 'rating':
-                return $item[ $column_name ];
+    function process_bulk_action()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'wesoftpress_random'; // do not forget about tables prefix
 
-            default:
-                return print_r( $item, true ) ;
+        if ('delete' === $this->current_action()) {
+            $ids = isset($_REQUEST['id']) ? $_REQUEST['id'] : array();
+            if (is_array($ids)) $ids = implode(',', $ids);
+
+            if (!empty($ids)) {
+                $wpdb->query("DELETE FROM $table_name WHERE id IN($ids)");
+            }
         }
+           
     }
 
-    public function load_notices() {
-        if ( isset( $_GET['result'] ) && $_GET['result'] == 'deleted' ) {
-            ?>
-            <div class="notice updated mwtsa-notice is-dismissible">
-                <p><?php _e( 'Search term(s) successfully deleted', 'search-analytics' ); ?></p>
-            </div>
-            <?php
-        }
-    }
+    /**
+     * [REQUIRED] This is the most important method
+     *
+     * It will get rows from database and prepare them to be showed in table
+     */
+    function prepare_items()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'wesoftpress_random'; // do not forget about tables prefix
 
+        $per_page = 99999999999999999; // constant, how much records will be shown per page
+
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = $this->get_sortable_columns();
+
+        // here we configure table headers, defined in our methods
+        $this->_column_headers = array($columns, $hidden, $sortable);
+
+        // [OPTIONAL] process bulk action if any
+        $this->process_bulk_action();
+
+        // will be used in pagination settings
+        $total_items = $wpdb->get_var("SELECT COUNT(id) FROM $table_name");
+
+        // prepare query params, as usual current page, order by and order direction
+        $paged = isset($_REQUEST['paged']) ? max(0, intval($_REQUEST['paged'] - 1) * $per_page) : 0;
+        $orderby = (isset($_REQUEST['orderby']) && in_array($_REQUEST['orderby'], array_keys($this->get_sortable_columns()))) ? $_REQUEST['orderby'] : 'term';
+        $order = (isset($_REQUEST['order']) && in_array($_REQUEST['order'], array('asc', 'desc'))) ? $_REQUEST['order'] : 'asc';
+
+        // [REQUIRED] define $items array
+        // notice that last argument is ARRAY_A, so we will retrieve array
+
+        if( ! empty( $_REQUEST['s'] ) ){
+            $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name WHERE term LIKE '%{$search}%' OR id LIKE '%{$search}%'", $per_page, $paged), ARRAY_A);
+        }else{
+            $this->items = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table_name ORDER BY $orderby $order LIMIT %d OFFSET %d", $per_page, $paged), ARRAY_A);
+        }
+
+        // [REQUIRED] configure pagination
+        $this->set_pagination_args(array(
+            'total_items' => $total_items, // total items defined above
+            'per_page' => $per_page, // per page constant defined at top of method
+            'total_pages' => ceil($total_items / $per_page) // calculate pages count
+        ));
+    }
 }
 ?>
